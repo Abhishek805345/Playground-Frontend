4512bcb4d27c2667402287cf6911acb97bbf0e8d
Abhishek805345 <jakharabhi49@gmail.com>
2026-09-21 22:12:01 +0530

    handling the the compnents of nav

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,10 +1,37 @@
-function Welcome(){
+import { Link } from "react-router-dom"
+
+export function Nav(){
   return (
-    <>
-    </>
+    <nav>
+      <div>
+        <Link to="">
+          Home
+        </Link>
+      </div>
+      <div>
+        <Link to="">
+          Events
+        </Link>
+      </div>
+      <div>
+        <Link to="">
+          Leader Board
+        </Link>
+      </div>
+      <div>
+        <Link to="">
+          store
+        </Link>
+      </div>
+      <div>
+        <Link to="">
+          Login
+        </Link>
+      </div>
+    </nav>
   )
 }
 
 export const sessionLoader=()=>{
-  
+  return 0;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import App from './App.jsx'
-import { sessionLoader } from './components/welcome.jsx';
+import { sessionLoader,Nav } from './components/welcome.jsx';
 
 const routes=createBrowserRouter([
   {
     path:"/",
-    element:<></>,
+    element:<Nav/>,
     loader:sessionLoader
   }
 ])
